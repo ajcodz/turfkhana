d913095ad8f36ec7570e99b176ffa870bf27387d
Ahsan Javed <ahsannjavaid@gmail.com>
2026-03-15 19:45:08 +0500

    refactor: get bookings controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import app from "./app";
 import dotenv from "dotenv";
+import bookingRoutes from "./routes/bookingRoutes";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT: number = parseInt(process.env.PORT as string) || 3000;
+
+const V1: string = "/api/v1";
+
+app.use(V1 + "/bookings", bookingRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
