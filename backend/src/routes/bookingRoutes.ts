19ac0d86203848a5ed852fbe23d28769da8eaf3b
Mohid <mmohid069@gmail.com>
2026-06-06 00:54:17 +0500

    feat: add booking delete controller

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-const { createBooking } = require('../controllers/booking/createBooking');
-const { getBookings } = require('../controllers/booking/getBookings');
+const { createBooking } = require("../controllers/booking/createBooking");
+const { getBookings } = require("../controllers/booking/getBookings");
+const { deleteBooking } = require("../controllers/booking/deleteBooking");
 
 const router = Router();
 
 router.post("/", createBooking);
 router.get("/", getBookings);
+router.delete("/:id", deleteBooking);
 
 export default router;
