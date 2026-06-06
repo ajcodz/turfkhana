8b239bcdd69a25a3ffd3c9341df3d5eddf9568e1
Mohid <mmohid069@gmail.com>
2026-06-06 15:38:21 +0500

    feat: add turf update controller

--- backend/src/routes/turfRoutes.ts
+++ backend/src/routes/turfRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getAllTurfs } from "../controllers/turf/getAllTurfs";
 import { createTurf } from "../controllers/turf/createTurf";
 import { getTurfById } from "../controllers/turf/getTurfById";
+import { updateTurf } from "../controllers/turf/updateTurf";
 
 const router = Router();
 
 router.post("/", createTurf);
 router.get("/", getAllTurfs);
 router.get("/:id", getTurfById);
+router.put("/:id", updateTurf);
 
 export default router;
