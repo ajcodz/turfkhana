70e78cbe3cd009b8a08b65c9886b8cb7a2dadd57
Mohid <mmohid069@gmail.com>
2026-06-06 12:35:39 +0500

    feat: add booking update controller

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 const { createBooking } = require("../controllers/booking/createBooking");
 const { getBookings } = require("../controllers/booking/getBookings");
 const { deleteBooking } = require("../controllers/booking/deleteBooking");
+const { updateBooking } = require("../controllers/booking/updateBooking");
 
 const router = Router();
 
 router.post("/", createBooking);
 router.get("/", getBookings);
 router.delete("/:id", deleteBooking);
+router.put("/:id", updateBooking);
 
 export default router;
