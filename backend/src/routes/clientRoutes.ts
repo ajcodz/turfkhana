820babb74c5c8e829b043d29d1e380fcdc79e917
Mohid <mmohid069@gmail.com>
2026-06-06 14:04:57 +0500

    feat: add client delete controller

--- backend/src/routes/clientRoutes.ts
+++ backend/src/routes/clientRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getClients } from "../controllers/client/getClients";
 import { createClient } from "../controllers/client/createClient";
 import { updateClient } from "../controllers/client/updateClient";
+import { deleteClient } from "../controllers/client/deleteClient";
 
 const router = Router();
 
 router.post("/", createClient);
 router.get("/", getClients);
 router.put("/:id", updateClient);
+router.delete("/:id", deleteClient);
 
 export default router;
