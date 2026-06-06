f4308ce441533f5081064c2ca3b15fe34880d546
Mohid <mmohid069@gmail.com>
2026-06-06 13:53:12 +0500

    feat: add client update controller

--- backend/src/routes/clientRoutes.ts
+++ backend/src/routes/clientRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getClients } from "../controllers/client/getClients";
 import { createClient } from "../controllers/client/createClient";
+import { updateClient } from "../controllers/client/updateClient";
 
 const router = Router();
 
 router.post("/", createClient);
 router.get("/", getClients);
+router.put("/:id", updateClient);
 
 export default router;
