955134c782654406fbb8aa49761cdbd3137cccab
Mohid <mmohid069@gmail.com>
2026-06-06 15:43:00 +0500

    feat: add turf delete controller

--- backend/src/routes/turfRoutes.ts
+++ backend/src/routes/turfRoutes.ts
@@ -3,6 +3,7 @@ import { getAllTurfs } from "../controllers/turf/getAllTurfs";
 import { createTurf } from "../controllers/turf/createTurf";
 import { getTurfById } from "../controllers/turf/getTurfById";
 import { updateTurf } from "../controllers/turf/updateTurf";
+import { deleteTurf } from "../controllers/turf/deleteTurf";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post("/", createTurf);
 router.get("/", getAllTurfs);
 router.get("/:id", getTurfById);
 router.put("/:id", updateTurf);
+router.delete("/:id", deleteTurf);
 
 export default router;
