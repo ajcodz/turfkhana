170e75958cf4f6b398762cb71a6aa07d6fc28a9a
Mohid <mmohid069@gmail.com>
2026-06-06 14:34:25 +0500

    feat: add owner update controller

--- backend/src/routes/ownerRoutes.ts
+++ backend/src/routes/ownerRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getOwners } from "../controllers/owner/getOwners";
 import { createOwner } from "../controllers/owner/createOwner";
+import { updateOwner } from "../controllers/owner/updateOwner";
 
 const router = Router();
 
 router.post("/", createOwner);
 router.get("/", getOwners);
+router.put("/:id", updateOwner);
 
 export default router;
